f93d4b2d790e4d85d63bbd4fa1d8bd732f995712
dinarfairus <dinarfs37@gmail.com>
2022-04-01 15:18:04 +0700

    button selected

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
 
 function App() {
     const CLIENT_ID = "1f31061be48a442d8d9de8ecfaaa4f13"
     const REDIRECT_URI = "http://localhost:3000"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    const SCOPE = 'playlist-modify-private'
     const RESPONSE_TYPE = "token"
 
-    const [token, setToken] = useState("")
+    const [userAccessToken, setToken] = useState("")
     const [search, setSearch] = useState("")
-    const [artists, setArtists] = useState([])
+    const [albums, setAlbums] = useState([])
 
     useEffect(() => {
         const hash = window.location.hash
@@ -33,11 +38,15 @@ function App() {
         window.localStorage.removeItem("token")
     }
 
-    const searchArtists = async (e) => {
+    const select = () => {
+
+    }
+
+    const searchAlbums = async (e) => {
         e.preventDefault()
-        const {data} = await axios.get("https://api.spotify.com/v1/search", {
+        const { data } = await axios.get("https://api.spotify.com/v1/search", {
             headers: {
-                Authorization: `Bearer ${token}`
+                Authorization: `Bearer ${userAccessToken}`
             },
             params: {
                 q: search,
@@ -45,14 +54,24 @@ function App() {
             }
         })
 
-        setArtists(data.artists.items)
+        setAlbums(data.artists.items)
     }
 
-    const renderArtists = () => {
-        return artists.map(artist => (
-            <div key={artist.id}>
-                {artist.images.length ? <img width={"35%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-                {artist.name}
+    const renderAlbums = () => {
+        return albums.map(artist => (
+            <div className="container-card">
+                <Card className="card" style={{ width: '20rem' }}>
+                    <Card.Body>
+                        <div
+                            key={artist.id}>
+                            <h3 >{artist.name}</h3>
+                            {artist.images.length ? <img width={"35%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
+                            <br></br>
+                            <Button className='btnSelected' variant="outline-primary" size="sm" onClick={logout}>Selected </Button>{' '}
+                        </div>
+                    </Card.Body>
+                </Card>
+                
             </div>
         ))
     }
@@ -60,21 +79,27 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
+
                 <h1>Spotify API</h1>
+                <div className="header">
+                    <Button className='btnLogout' variant="danger" onClick={logout}>Logout</Button>
+                </div>
 
-                {token ?
-                    <form class="search" onSubmit={searchArtists}>
-                        <input type="text" onChange={e => setSearch(e.target.value)}/>
-                        <button type={"submit"}>Search</button>
-                    </form>
-
-                    : <br></br>
+                {userAccessToken ?
+                    <div className="search">
+                        <Form style={{ width: '20rem' }} onSubmit={searchAlbums}>
+                            <Form.Control type="text" placeholder="Search Your Music" onChange={e => setSearch(e.target.value)} />
+                            <Button className="btnSearch" as="input" type="submit" value="Search" />{' '}
+                        </Form>
+                    </div>
+                    : <div></div>
                 }
-                {!token ?
-                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                {!userAccessToken ?
+                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}>Login
                         to Spotify</a>
-                    : <button onClick={logout}>Logout</button>}
-                {renderArtists()}
+                    : <div></div>}
+
+                {renderAlbums()}
 
             </header>
         </div>
